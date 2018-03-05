9b3537a1b1328993ae7ccd416592dc3198022a1b
kuswandanu <kuswandanu@gmail.com>
2018-03-06 04:09:51 +0700

    add process.env.PORT

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ server.get('/', function (req, res, next) {
     return next();
 });
 
-server.listen(8080, function () {
+server.listen(process.env.PORT || 8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
